35572b9d25afde1ee21ece12bee484d89156a4b0
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2024-10-09 09:31:17 +0530

    Refactor profile page CSS to display profile actions with a gap and added email  verification

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,5 +4,5 @@ from config import db
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
         
--- frontend/src/Pages/User_Pages/User_profile.jsx
+++ frontend/src/Pages/User_Pages/User_profile.jsx
@@ -68,6 +68,25 @@ const User_profile = () => {
     }
   };
 
+  const handel_email_vderification = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${get_access_token}`,
+      },
+    };
+    const response = await fetch(
+      `${apiUrl}/verification/send_verification_email`,
+      options
+    );
+    const data = await response.json();
+    if (response.status === 200) {
+      toast.success(data.message);
+    } else {
+      toast.error(data.message);
+    }
+  };
+
   useEffect(() => {
     get_data();
   }, []);
@@ -192,6 +211,13 @@ const User_profile = () => {
                 Edit Profile
               </button>
               <button className="change-password">Change Password</button>
+
+              <button
+                className="change-password"
+                onClick={handel_email_vderification}
+              >
+                Verify Email
+              </button>
             </div>
           </div>
         </>
