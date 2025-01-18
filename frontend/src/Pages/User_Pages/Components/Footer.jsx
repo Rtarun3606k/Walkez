ae163947a7c206028e86073eae0266733da6c4b1
VaibhavBhat7 <vaibhav762005@gmail.com>
2025-01-18 11:11:58 +0530

    footer

--- frontend/src/Pages/User_Pages/Components/Footer.jsx
+++ frontend/src/Pages/User_Pages/Components/Footer.jsx
@@ -6,38 +6,12 @@ const Footer = () => {
   return (
     <footer className="main-footer ">
       <div className="footer-container">
-        <div className="footer-legend">
-          <h3>Legend</h3>
-          <table>
-            <tbody>
-              <tr>
-                <td className="t1">1. Humans</td>
-                <td className="t6">6. Cables</td>
-              </tr>
-              <tr>
-                <td className="t2">2. Vehicles</td>
-                <td className="t7">7. Water</td>
-              </tr>
-              <tr>
-                <td className="t3">3. Garbage</td>
-                <td className="t8">8. Broken Pavement</td>
-              </tr>
-              <tr>
-                <td className="t4">4. Stores</td>
-                <td className="t9">9. Crossing</td>
-              </tr>
-              <tr>
-                <td className="t5">5. Animals</td>
-                <td className="t10">10. Other</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        
 
         <div className="footer-contact">
           <h3>Contact us</h3>
           <p>
-            <b>Email:</b> joemama@gmail.com
+            <b>Email:</b> walkezwalk@gmail.com
           </p>
           <p>
             <b>Phone:</b> 696969669
@@ -50,13 +24,13 @@ const Footer = () => {
         <div className="footer-socials">
           <h3>Socials</h3>
           <p>
-            <a href="https://instagram.com">Instagram</a>
+            <a href="https://www.instagram.com/invites/contact/?igsh=1fbtk2hmxuzk5&utm_content=vxccn7e">Instagram</a>
           </p>
           <p>
-            <a href="https://facebook.com">Facebook</a>
+            <a href="https://youtube.com/@walkez?si=Jp9rAz3xx5hc9f68">Youtube</a>
           </p>
           <p>
-            <a href="https://twitter.com">Twitter</a>
+            <a href="https://x.com/walkezwalk?t=Skd97YZFBYCTp5ZW_Uq-xw&s=08">Twitter</a>
           </p>
         </div>
       </div>
