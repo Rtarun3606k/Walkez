fe339c021f1df0283531cd0ba9bfa260c7881d44
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2025-03-12 20:15:16 +0530

    Enhance map interface with custom scrollbar styles, pop-up functionality for markers, and improved error handling in data fetching

--- frontend/src/Pages/User_Pages/Components/CustomMarker.jsx
+++ frontend/src/Pages/User_Pages/Components/CustomMarker.jsx
@@ -3,6 +3,15 @@ import * as atlas from "azure-maps-control"; // Import Azure Maps SDK
 
 const CustomMarker = ({ mapRef, longitude, latitude, imgUrl }) => {
   const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [popUp, setPopUp] = useState(true);
+
+  const PopUpDiv = () => {
+    return (
+      <div className="absolute z-50">
+        <p>i am a popUp</p>
+      </div>
+    );
+  };
 
   useEffect(() => {
     if (mapRef?.current && longitude !== null && latitude !== null) {
@@ -19,7 +28,14 @@ const CustomMarker = ({ mapRef, longitude, latitude, imgUrl }) => {
   }, [longitude, latitude, mapRef]);
 
   return (
-    <div className="absolute z-50 w-10" style={position}>
+    <div
+      className="absolute z-50 w-10"
+      style={position}
+      onClick={() => {
+        console.log(imgUrl);
+        setPopUp(true);
+      }}
+    >
       {/* Marker Box */}
       <div className="bg-red-500 text-white p-4 border-2 border-black rounded-lg shadow-lg flex justify-center items-center">
         <img
@@ -31,6 +47,8 @@ const CustomMarker = ({ mapRef, longitude, latitude, imgUrl }) => {
 
       {/* Triangle Pointer (Arrow) */}
       <div className="absolute left-1/2 -bottom-3 w-0 h-0 border-l-8 border-r-8 border-t-8 border-transparent border-t-black transform -translate-x-1/2"></div>
+
+      {popUp ? <PopUpDiv /> : null}
     </div>
   );
 };
